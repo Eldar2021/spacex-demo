39c9dec4dca22f24eab5d5cb7ecd40339dea352c
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-24 08:36:28 +0600

    fixed exports

--- packages/crew_member_repository/lib/crew_member_repository.dart
+++ packages/crew_member_repository/lib/crew_member_repository.dart
@@ -1,3 +1,5 @@
 library crew_member_repository;
 
 export 'src/crew_member_repository.dart';
+export 'src/exception/exception.dart';
+export 'src/models/models.dart';
--- packages/rocket_repository/lib/rocket_repository.dart
+++ packages/rocket_repository/lib/rocket_repository.dart
@@ -1,3 +1,5 @@
 library rocket_repository;
 
+export 'src/exception/exception.dart';
+export 'src/models/models.dart';
 export 'src/rocket_repository.dart';
